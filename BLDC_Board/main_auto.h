827c3f665364f586b9e863d8a5484e6c39795bd9
Pna2791 <pna2791@gmail.com>
2026-08-02 22:00:33 +0700

    remove unused blocks

--- BLDC_Board/main_auto.h
+++ BLDC_Board/main_auto.h
@@ -26,21 +26,12 @@ BluetoothSerial SerialBT;
 Encoder     left_encoder(13);
 Encoder     right_encoder(18);
 
-BLDC_Motor  slider_motor(27, 26, 14, 1, 0, 1);
-Encoder     slider_encoder(34, 35);
-
 //           pwm, dir, brake, speed, dir, brake
 BLDC_Motor motor_left( 16,  5, 17, 1, 0, 1);
 BLDC_Motor motor_right(22, 23, 19, 1, 1, 1);
 
-// PIDController   slider_pid(4, 0.004, 0.01, -150, 255, 20);   // P, I, D, max_speed
-PIDController   slider_pid(2, 0.002, 0.01, -150, 255, 20);   // P, I, D, max_speed
-
 PIDController   forward_pid(10, 2, 1, -192, 192); // 315rpm speed 100-150
 PIDController   rotate_pid(2, 0.0, 0.15, -255, 255); // 315rpm speed 100-150
-
-#define MAX_HEIGHT 300
-BLDC_Servo slider_servo(slider_motor, slider_encoder, slider_pid, 50); // steps/mm
 
 
 bool servo_enable = false;
@@ -56,14 +47,11 @@ void setup() {
     SerialBT.begin(ROBOT_NAME); // Set the Bluetooth device name
     Serial2.begin(115200, SERIAL_8N1, 2, 15); // RX, TX use for Hi229
 
-    slider_encoder.begin();
     left_encoder.begin();
     right_encoder.begin();
-    slider_servo.limit(0, MAX_HEIGHT);
 
     motor_left.stop();
     motor_right.stop();
-    slider_motor.stop();
 
     delay(1000);
     Serial.println("Started");
@@ -77,17 +65,13 @@ void forward_command(String command){
 
 void show_encoder(){
     static long next_update = millis();
-    static long prev_pos = 0;
 
     if(millis() > next_update){
-        long slider_pos = slider_encoder.getCount();
         long left_pos = left_encoder.getCount();
         long right_pos = right_encoder.getCount();
-        String message = String(slider_pos - prev_pos) + " \t" + String(slider_pos)
-                       + " \t" + String(left_pos) + " \t" + String(right_pos);
+        String message = String(left_pos) + " \t" + String(right_pos);
         Serial.println(message);
 
-        prev_pos = slider_pos;
         next_update += 1000;
     }
 }
@@ -97,7 +81,6 @@ void update_servo(){
     static long next_update = millis();
     if(millis() > next_update){
         if(servo_enable){
-            slider_servo.run();
             show_encoder();
         }
 
@@ -155,14 +138,6 @@ void my_delay(int value){
     }
 }
 
-
-void check_servo(int speed){
-    servo_enable = false;
-    slider_motor.setSpeed(speed);
-    my_delay(5);
-    servo_enable = true;
-}
-
 void move_wheel(int dir){
     Serial.println("Move " + String(dir));
     if(dir == 0){
@@ -211,105 +186,6 @@ void move_wheel(int dir){
         return;
     }
 }
-
-
-void process_hand(char ch){
-    if(servo_enable){
-        int delta = 0;
-        if(ch == '*')   delta =  50;
-        if(ch == '/')   delta = -50;
-        if(ch == '+')   delta =  5;
-        if(ch == '-')   delta = -5;
-        if(delta > 0){
-            slider_motor.setSpeed(-10);
-            my_delay(1);
-        }else{
-            slider_motor.setSpeed(10);
-            my_delay(1);
-        }
-        slider_servo.move_position_mm(delta);
-    }else{
-        if(ch == '0')   slider_motor.stop();
-        if(ch == '*')   slider_motor.setSpeed(255);
-        if(ch == '/')   slider_motor.setSpeed(-170);
-        if(ch == '+')   slider_motor.setSpeed(100);
-        if(ch == '-')   slider_motor.setSpeed(-80);
-    }
-    if(ch == 'E')   servo_enable = true;
-    if(ch == 'D'){
-        servo_enable = false;
-        slider_motor.stop();
-    }
-    if(ch == 'R'){
-        slider_servo.hard_reset();
-        Serial.println("Reset hand at height is 0");
-    }
-}
-
-
-void process_hand_servo(int value){
-    if(value < 0)   return;
-    if(value > MAX_HEIGHT) return;
-
-    if(servo_enable){
-        slider_servo.goto_position_mm(value);
-    }
-}
-
-
-void auto_reset(){
-    Serial.println("Auto reset");
-
-    servo_enable = false;
-    #if defined(ROBOT_NAP_2)
-        slider_motor.setSpeed(-20);
-        my_delay(4000);
-    #else
-        slider_motor.setSpeed(-30);
-        my_delay(1000);
-    #endif
-    slider_motor.stop();
-    my_delay(500);
-
-    slider_servo.reset(-10);
-    servo_enable = true;
-}
-
-
-#define auto_take_delay 1000
-// NAP's Combo
-void prepare_D30(){
-    forward_command("OA1");
-    slider_servo.goto_position_mm(50);
-}
-void take_D30(){
-    check_servo(20);
-    slider_servo.goto_position_mm(0);
-    my_delay(auto_take_delay);
-    
-    slider_servo.goto_position_mm(100);
-}
-
-void prepare_D40(){
-    forward_command("OA1");
-    slider_servo.goto_position_mm(150);
-}
-void take_D40(){
-    check_servo(20);
-    slider_servo.goto_position_mm(100);
-    my_delay(auto_take_delay);
-    
-    slider_servo.goto_position_mm(200);
-}
-
-
-void process_vaccum(char ch){
-    if(ch == '0')   forward_command("OA0");
-    if(ch == '1')   forward_command("OA1");
-}
-
-
-
 
 
 #define DEBUG
@@ -380,6 +256,68 @@ void auto_forward(int distance){
     motor_left.stop();
     motor_right.stop();
     Serial.println("Finish forward: " + String(distance));
+    Serial.println("Left current pos: " + String(left_encoder.getCount()));
+    Serial.println("Right current pos: " + String(right_encoder.getCount()));
+}
+
+// New function: auto_backward for automatic mode (Robocon), 2 motor with IMU
+void auto_backward(int distance){
+    Serial.println("Auto backward: " + String(distance));
+    Serial.println("step_per_mm: " + String(step_per_mm));
+    forward_pid.reset();
+    float delta_plush = step_per_mm * (distance-brake_distance);
+    long left_target = left_encoder.getCount() - delta_plush;
+    long right_target = right_encoder.getCount() - delta_plush;
+    Serial.println("Left target pos: " + String(left_target));
+    Serial.println("Right target pos: " + String(right_target));
+
+    int auto_speed = -auto_forward_speed; // Negative speed for backward
+    motor_left.setSpeed(auto_speed);
+    motor_right.setSpeed(auto_speed);
+
+    bool is_normal_speed = true;
+    float delta_slowdown = step_per_mm * slowdown_distance;
+    int left_pos_slowdown = left_target + delta_slowdown;   // + (since both are negative direction)
+    int right_pos_slowdown = right_target + delta_slowdown;
+    // In backward, we approach target when encoder counts decrease to <= target
+    while(left_encoder.getCount() > left_target || right_encoder.getCount() > right_target){
+        if (
+            is_normal_speed
+            && (left_encoder.getCount() < left_pos_slowdown)
+            && (right_encoder.getCount() < right_pos_slowdown)
+        ){
+            auto_speed = -auto_forward_speed * 0.4;
+            is_normal_speed = false;
+        }
+
+        my_loop();
+        if(emergency_stop){
+            Serial.println("Emergency stopped");
+            motor_left.stop();
+            motor_right.stop();
+            return;
+        }
+
+        int direction = get_direction(Serial2);
+        if(direction != 0xFFF){
+            Serial.println("Ang: " + String(direction));
+            direction = standard_dir(target_dir, direction);
+
+            float delta_value = forward_pid.compute(target_dir, direction)/255;
+            #ifdef DEBUG
+                String message = String(delta_value*10) + '\t' + String(target_dir-direction);
+                SerialBT.println(message);
+            #endif
+
+            // For backward, reverse the correction (swap +/-)
+            motor_left.setSpeed(auto_speed * (1 + delta_value));
+            motor_right.setSpeed(auto_speed * (1 - delta_value));
+        }
+    }
+
+    motor_left.stop();
+    motor_right.stop();
+    Serial.println("Finish backward: " + String(distance));
     Serial.println("Left current pos: " + String(left_encoder.getCount()));
     Serial.println("Right current pos: " + String(right_encoder.getCount()));
 }
@@ -463,15 +401,11 @@ void reset_direction(HardwareSerial &serialPort = Serial){
 
 void process_combo(int value){
     if(value == 0)  reset_direction(Serial2);
-    if(value == 11) prepare_D30();
-    if(value == 12) take_D30();
-    if(value == 13) prepare_D40();
-    if(value == 14) take_D40();
-
     if(value == 16) forward_command("OA0");
     if(value == 17) forward_command("O21");
 
     if(value == 20) auto_forward(1200);
+    if(value == 25) auto_backward(1200);
     if(value == 29) auto_forward(4000);
     if(value == 21) rote_CCW();
     if(value == 22) rote_CW();
@@ -527,12 +461,6 @@ void processSerialCommand(String command) {
         return;
     }
 
-    if(prefix == 'H'){  // Chassis direction
-        int value = command.substring(1).toInt();
-        process_hand_servo(value);
-        return;
-    }
-
     if(prefix == 'E'){
         emergency_stop = true;
         return;
@@ -544,22 +472,12 @@ void processSerialCommand(String command) {
         return;
     }
 
-    if(prefix == 'B'){  // Body
-        process_hand(command.charAt(1));
-        return;
-    }
-
     if(prefix == 'O'){  // ON/OFF
         if(command.length() == 3)
             forward_command(command);
         return;
     }
-
-    if(prefix == 'T'){  // ON/OFF
-        process_vaccum(command.charAt(1));
-        return;
-    }
-
+    
     if(prefix == 'k'){  // update_PID
         update_k_PID(command.substring(1));
         return;
